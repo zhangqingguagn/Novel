380a311ecb5101848c9aef4fdd08f14edb32e0f1
zhangqingguang <zhangqingguang120@163.com>
2017-09-07 17:30:41 +0800

    SQLite3 测试

---
+++ SqliteTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqliteTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db =  new testdb())
+            {
+                Student stu = new Student()
+                {
+                    name = "小张",
+                    sex = "男"
+                };
+                db.Students.Add(stu);
+                db.SaveChanges();
+
+                var students = db.Students.ToList();
+            }
+        }
+    }
+}
---
+++ SqliteTest/testdb.cs
@@ -0,0 +1,34 @@
+﻿namespace SqliteTest
+{
+    using System;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class testdb : DbContext
+    {
+        //您的上下文已配置为从您的应用程序的配置文件(App.config 或 Web.config)
+        //使用“testdb”连接字符串。默认情况下，此连接字符串针对您的 LocalDb 实例上的
+        //“SqliteTest.testdb”数据库。
+        // 
+        //如果您想要针对其他数据库和/或数据库提供程序，请在应用程序配置文件中修改“testdb”
+        //连接字符串。
+        public testdb()
+            : base("name=testdb")
+        {
+            Database.SetInitializer<testdb>(null);
+        }
+
+        //为您要在模型中包含的每种实体类型都添加 DbSet。有关配置和使用 Code First  模型
+        //的详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=390109。
+
+        public virtual DbSet<Student> Students { get; set; }
+    }
+    [Table("student", Schema = "dbo")]
+    public class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string sex { get; set; }
+    }
+}
